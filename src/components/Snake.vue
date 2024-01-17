97727d905b1b30d10139e350a1785f5a685e2ff4
ai-strings <adrstrings@gmail.com>
2024-01-17 17:22:07 +0300

    !fix game over on fast key press

--- src/components/Snake.vue
+++ src/components/Snake.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="game">
+    <div class="score">
+      <h5>{{ score }}</h5>
+      <h5>{{ attempts }}</h5>
+    </div>
     <canvas
       ref="gameField"
       id="game-field"
@@ -7,6 +11,7 @@
       height="500"
       style="border: 1px solid white"
     ></canvas>
+    <button class="contrast control-button" @click="restartGame">Restart</button>
     <h3 v-show="gameOver">You lost</h3>
   </div>
 </template>
@@ -18,20 +23,23 @@ const gameField = ref<HTMLCanvasElement | null>(null)
 const gameInterval = ref<number | null>(null)
 const gameIsRunning = ref<boolean>(false)
 const gameOver = ref<boolean>(false)
+const isPaused = ref<boolean>(false)
 const score = ref<number>(0)
+const attempts = ref<number>(0)
+const hasMoved = ref<boolean>(false)
 let ctx: CanvasRenderingContext2D
 const unitSize = 25
 let xVelocity = unitSize
 let yVelocity = 0
 
-const gameTick = 250
+const gameTick = 100
 let gameFieldWidth
 let gameFieldHeight
 
 let foodX
 let foodY
 
-const snake = [
+let snake = [
   { x: unitSize * 3, y: 0 },
   { x: unitSize * 2, y: 0 },
   { x: unitSize, y: 0 },
@@ -43,7 +51,7 @@ onMounted(async () => {
     ctx = gameField.value?.getContext('2d')
     gameFieldWidth = gameField.value?.width
     gameFieldHeight = gameField.value?.height
-    window.addEventListener('keydown', (e) => debounce(handleKeys)(e))
+    window.addEventListener('keydown', (e) => handleKeys(e))
     clearCanvas()
     createFood()
     gameIsRunning.value = true
@@ -52,25 +60,17 @@ onMounted(async () => {
   })
 })
 
-function debounce(fn) {
-  let timeout
-  return function (...args) {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => fn(...args), gameTick / 3)
-  }
-}
-
 function RandCoordinate(min, max) {
   return Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize
 }
 function startGame() {
   gameInterval.value = setInterval(() => {
-    if (gameIsRunning.value) {
+    if (gameIsRunning.value && !isPaused.value) {
       clearCanvas()
       drawFood()
       moveSnake()
       drawSnake()
-    } else {
+    } else if (!gameIsRunning.value) {
       clearInterval(gameInterval.value as number)
     }
   }, gameTick)
@@ -85,6 +85,7 @@ function drawSnake() {
   })
 }
 function moveSnake() {
+  hasMoved.value = false
   ctx.fillStyle = '#1db953'
   ctx.strokeStyle = '#eaeaea'
   const head = { x: snake[0].x + xVelocity, y: snake[0].y + yVelocity }
@@ -110,8 +111,27 @@ function drawFood() {
 function clearCanvas() {
   ctx.clearRect(0, 0, gameFieldWidth, gameFieldHeight)
 }
-
+function restartGame() {
+  clearInterval(gameInterval.value as number)
+  snake = [
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 }
+  ]
+  score.value = 0
+  attempts.value++
+  clearCanvas()
+  createFood()
+  gameIsRunning.value = true
+  startGame()
+}
 function handleKeys(e: KeyboardEvent) {
+  console.log(e)
+  if (e.code === 'Space') {
+    isPaused.value = !isPaused.value
+  }
+  if (hasMoved.value) return
   const goingUp = yVelocity == -unitSize
   const goingDown = yVelocity == unitSize
   const goingRight = xVelocity == unitSize
@@ -121,18 +141,22 @@ function handleKeys(e: KeyboardEvent) {
     case e.key === 'ArrowUp' && !goingDown:
       xVelocity = 0
       yVelocity = -unitSize
+      hasMoved.value = true
       break
     case e.key === 'ArrowDown' && !goingUp:
       xVelocity = 0
       yVelocity = unitSize
+      hasMoved.value = true
       break
     case e.key === 'ArrowLeft' && !goingRight:
       xVelocity = -unitSize
       yVelocity = 0
+      hasMoved.value = true
       break
     case e.key === 'ArrowRight' && !goingLeft:
       xVelocity = unitSize
       yVelocity = 0
+      hasMoved.value = true
       break
   }
 }
@@ -152,4 +176,25 @@ function checkGameOver(head) {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.game {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 90vh;
+  padding: 20px 0;
+}
+.score {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 500px;
+}
+.control-button {
+  margin: 25px 0;
+  min-width: 150px;
+  width: 25%;
+}
+</style>
